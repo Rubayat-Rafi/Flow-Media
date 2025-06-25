5d8904dd203a99908841869d692b17386ac7ec1f
hirock0 <hirockdutta0@gmail.com>
2025-06-25 22:48:29 +0600

    all three palyer completed

--- frontend/src/components/MianContent/MainContent.jsx
+++ frontend/src/components/MianContent/MainContent.jsx
@@ -2,7 +2,7 @@ import HlsPlayer from "../HlsPlayer/HlsPlayer";
 const MainContent = () => {
   return (
     <section className="h-full bg-[var(--secondary)] rounded-md shadow-lg p-4">
-      <HlsPlayer src="" />
+      <HlsPlayer src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" />
     </section>
   );
 };
