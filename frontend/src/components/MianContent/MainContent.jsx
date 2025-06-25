0eb07a8e1ebce06d3b726344adfcc220177ad03d
hirock0 <hirockdutta0@gmail.com>
2025-06-25 18:47:57 +0600

    change ui created

--- frontend/src/components/MianContent/MainContent.jsx
+++ frontend/src/components/MianContent/MainContent.jsx
@@ -2,7 +2,7 @@ import HlsPlayer from "../HlsPlayer/HlsPlayer";
 const MainContent = () => {
   return (
     <section className="h-full bg-[var(--secondary)] rounded-md shadow-lg p-4">
-      <HlsPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+      <HlsPlayer src="" />
     </section>
   );
 };
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,24 @@
-
-const Sidebar = ({sidebarContent}) => {
-
-    console.log('sidebarContent:', sidebarContent);
-    return (
-    <aside className='bg-[var(--secondary)] p-4 rounded-md shadow-lg h-full'>
+import WatchBtn from "../ui/btns/WatchBtn";
+const Sidebar = ({ sidebarContent, chennels }) => {
+  return (
+    <aside className="bg-[var(--secondary)]  p-4 rounded-md shadow-lg h-full">
       <h2 className="font-bold text-white mb-2">Selected Category:</h2>
       <p>{sidebarContent ?? "None selected"}</p>
+
+      <div className=" space-y-3">
+        {chennels?.map((ch, idx) => (
+          <div
+            key={idx}
+            className=" border-b py-2 flex items-center justify-between"
+          >
+            <h1>{ch?.logo}</h1>
+            <h1>{ch.name}</h1>
+            <WatchBtn />
+          </div>
+        ))}
+      </div>
     </aside>
-    );
+  );
 };
 
 export default Sidebar;
