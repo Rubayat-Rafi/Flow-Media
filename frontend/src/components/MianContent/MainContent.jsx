ebee965d2798d50dd86a0db241fcb329833b471a
hirock0 <hirockdutta0@gmail.com>
2025-06-25 17:53:24 +0600

    hls player added

--- frontend/src/components/MianContent/MainContent.jsx
+++ frontend/src/components/MianContent/MainContent.jsx
@@ -1,11 +1,10 @@
-
-
+import HlsPlayer from "../HlsPlayer/HlsPlayer";
 const MainContent = () => {
-    return (
-        <section className='h-full bg-[var(--secondary)] rounded-md shadow-lg p-4'>
-            <h1>Main Content</h1>
-        </section>
-    );
+  return (
+    <section className="h-full bg-[var(--secondary)] rounded-md shadow-lg p-4">
+      <HlsPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+    </section>
+  );
 };
 
 export default MainContent;
