37d2a6d28452e443d2a83abdbc5f41b9a756a8e1
rubayat-rafi <rubayatrafi522@gmail.com>
2025-06-25 19:53:20 +0600

    sidebar design

--- frontend/src/components/MianContent/MainContent.jsx
+++ frontend/src/components/MianContent/MainContent.jsx
@@ -1,8 +1,8 @@
 import HlsPlayer from "../HlsPlayer/HlsPlayer";
 const MainContent = () => {
   return (
-    <section className="h-full bg-[var(--secondary)] rounded-md shadow-lg p-4">
-      <HlsPlayer src="" />
+    <section className="h-full bg-[var(--secondary)] rounded-md shadow-lg p-8">
+      <HlsPlayer src="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8" />
     </section>
   );
 };
