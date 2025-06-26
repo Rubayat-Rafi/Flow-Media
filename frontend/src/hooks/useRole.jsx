06a7ffafd5fb8746c967955b727f40e2caf73ae9
rubayat-rafi <rubayatrafi522@gmail.com>
2025-06-26 20:50:57 +0600

    query

--- frontend/src/hooks/useRole.jsx
+++ frontend/src/hooks/useRole.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useAuth } from "./useAuth";
-import { useAxiosSecure } from "./useAxiosSecure";
+import useAxiosSecure from "./useAxiosSecure";
+
 
 const useRole = () => {
   const axiosSecure = useAxiosSecure();
@@ -10,7 +11,7 @@ const useRole = () => {
     queryKey: ["role", user?.email],
     enabled: !loading && !!user?.email,
     queryFn: async () => {
-      const { data } = await axiosSecure.get(`/user/role/${user?.email}`);
+      const { data } = await axiosSecure.get(`/api/user/role/${user?.email}`);
       return data.role;
     },
   });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,16 @@ import { router } from "./routes/Routes";
 import AuthProvider from "./provider/AuthProvider";
 import { Provider } from "react-redux";
 import store from "./utils/redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <AuthProvider>
-        <QueryClientProvider client={QueryClient}>
+        <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
         </QueryClientProvider>
       </AuthProvider>
