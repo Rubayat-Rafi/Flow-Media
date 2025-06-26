da378ef0f316786f76db6b606d20a65fb4ba82eb
hirock0 <hirockdutta0@gmail.com>
2025-06-26 11:11:39 +0600

    retouch entry poin

--- backend/index.js
+++ backend/index.js
@@ -6,29 +6,21 @@ const subsCriptionRoutes = require("./routes/subscription.routes.js");
 const userRoutes = require("./routes/user.routes.js");
 const app = express();
 const port = process.env.PORT || 5000;
-
-// middleware
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 async function run() {
   try {
-
-    // routes
     app.use("/api/user", subsCriptionRoutes);
     app.use("/api/user", userRoutes);
-
-    // default route
     app.get("/", (req, res) => {
-      res.send("🌐 Flow Media Backend Running");
+      res.send("Flow Media Backend Running");
     });
-
     app.listen(port, () => {
-      console.log(`🚀 Backend server is running on port: ${port}`);
+      console.log(`Backend server is running on port: ${port}`);
     });
   } catch (error) {
-    console.error("❌ Failed to connect to MongoDB:", error.message);
+    console.error("Failed to connect to MongoDB:", error.message);
   }
 }
 
