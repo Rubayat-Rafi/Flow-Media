44e3d220ffe9c687856bfaafba950946a22711c6
hirock0 <hirockdutta0@gmail.com>
2025-07-07 21:10:43 +0600

    shedule time done

--- frontend/src/components/TimeZone/ConvertMatchTimeByTimeZone.js
+++ frontend/src/components/TimeZone/ConvertMatchTimeByTimeZone.js
@@ -3,16 +3,12 @@ export const convertMatchTimeByTimeZone = (timeString, timeZone) => {
   const offsetMatch = timeZone.match(/GMT([+-]?\d+)/);
   const offset = offsetMatch ? parseInt(offsetMatch[1], 10) : 0;
   let [hours, minutes] = timeString.split(":").map(Number);
-  hours -= 12;
-  if (hours < 0) {
-    hours += 24;
-  }
-  const utcDate = new Date(Date.UTC(1970, 0, 1, hours, minutes));
+  const validHour = hours > 12 ? hours - 12 : hours;
+  const utcDate = new Date(Date.UTC(1970, 0, 1, validHour, minutes));
   const shiftedDate = new Date(utcDate.getTime() + offset * 60 * 60 * 1000);
   return shiftedDate.toLocaleTimeString("en-US", {
-    hour: "numeric",
+    hour: "2-digit",
     minute: "2-digit",
     hour12: true,
-    timeZone: "UTC",
   });
 };
--- frontend/src/components/TimeZone/TimeZone.js
+++ frontend/src/components/TimeZone/TimeZone.js
@@ -1,12 +1,10 @@
 export const UpdateTime = (timeZone) => {
   const offsetMatch = timeZone?.match(/GMT([+-]?\d+)/);
   const offset = offsetMatch ? parseInt(offsetMatch[1], 10) : 0;
-
   const utc = new Date(
     new Date().getTime() + new Date().getTimezoneOffset() * 60000
   );
   const timezoneDate = new Date(utc.getTime() + offset * 3600000);
-
   return timezoneDate.toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
