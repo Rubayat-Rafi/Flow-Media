2fe43875225970f39e7c29b7806870d9e0477e38
hirock0 <hirockdutta0@gmail.com>
2025-07-08 20:57:54 +0600

    index folder recorrect

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
+// Import routes
 const userRoutes = require("./routes/user.routes.js");
 const categoryRoutes = require("./routes/category.routes.js");
 const paymentRoutes = require("./routes/payments.route.js");
-const freeTrialRoutes = require("./routes/freeTrial.routes.js")
+const freeTrialRoutes = require("./routes/freeTrial.routes.js");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
+// Middlewares
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-async function run() {
-  try {
-    app.use("/api/user", userRoutes);
-    app.use("/api/users", userRoutes);
-    app.use("/api", categoryRoutes);
-    app.use("/api/category", categoryRoutes);
-    app.use("/api/payment", paymentRoutes);
-    app.use("/api/free-trial",freeTrialRoutes)
-    app.listen(port, () => {
-      console.log(`Backend server is running on port: ${port}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error.message);
-  }
-}
 
-run().catch(console.dir);
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes); // optional
+app.use("/api", categoryRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/free-trial", freeTrialRoutes);
+
+// Root route
+app.get("/", (req, res) => {
+  const message = 'It works!';
+  const version = 'NodeJS ' + process.versions.node;
+  const response = [message, version].join('\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.send(response);
+});
+
+module.exports = app;
