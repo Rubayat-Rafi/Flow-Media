be85163f8b7407e69be5c8b95408a2120584c54d
rubayat-rafi <rubayatrafi522@gmail.com>
2025-07-15 00:59:06 +0600

    try to optimize

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 export const saveUser = async (user) => {
-
-
-  console.time(user)
   await axios.post(`${import.meta.env.VITE_FLOW_MRDIA_API}/api/user/signup`, {
     name: user.name,
     email: user.email,
